9ad6884e9487c97ae16542fb5382d723bf9d7cae
Denys Burdin <denrowing@gmail.com>
2021-11-15 01:10:18 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+// зробити запит на ендпоінт https://jsonplaceholder.typicode.com/users
+//   відобразити всіх юзерів.
+//   Компоненти App-Users->User
+//
+//
+// зробити запит на ендпоінт https://jsonplaceholder.typicode.com/posts
+//   відобразити всі пости.
+//   Компоненти App-Posts->Post
+
+
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'angular-base';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
