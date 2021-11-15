bcab0ea0983652ddf6f02b8b4a7eac671cc86518
Denys Burdin <denrowing@gmail.com>
2021-11-15 19:43:13 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ import {Component} from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    UsersComponent,
+    PostComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
