6a0f2fdbe85af4a336a8a62d8ff352be77f0116a
Denys Burdin <denrowing@gmail.com>
2021-11-22 12:01:55 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
-// зробити запит на ендпоінт https://jsonplaceholder.typicode.com/users
-//   відобразити всіх юзерів.
-//   Компоненти App-Users->User
-//
-//
-// зробити запит на ендпоінт https://jsonplaceholder.typicode.com/posts
-//   відобразити всі пости.
-//   Компоненти App-Posts->Post
-
+// создать переход на url /posts
+// при переходе  отобразить все title постов с jsonplaceholder.
+//   У каждого поста есть кнопка, при клике на которую отображается детальная информация про пост
 
 import {Component} from '@angular/core';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import {RouterModule} from "@angular/router";
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    PostsComponent,
+    CommentsComponent,
+    UserDetailsComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'users',
+        component: UsersComponent,
+        children: [
+          {path: ':id', component: UserDetailsComponent}
+        ]
+      },
+
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
