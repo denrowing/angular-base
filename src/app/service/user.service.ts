5b87191dd830a1cb8e243eca2469d85a0582c839
Denys Burdin <denrowing@gmail.com>
2021-11-23 21:12:08 +0200

    init commit

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url = 'https://jsonplaceholder.typicode.com/users';
 
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getUsers(): Observable<any[]> {
+    return this.httpClient.any[] > (this.url);
+  }
+
+  getUser(id: number) {
+    return this.httpClient.getUser(this.url + '/' + id);
+  }
 
 }
