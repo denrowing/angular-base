8abef12f1fd943d4ce9d5617dbb56c2bf3188710
Denys Burdin <denrowing@gmail.com>
2021-11-24 14:59:49 +0200

    init commit

---
+++ src/app/service/post.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../models/Post";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  getPosts(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.url);
+  }
+  getPost(id: number) {
+    return this.httpClient.get<Post[]>(this.url + '/posts?userId=' + id);
+  }
+}
